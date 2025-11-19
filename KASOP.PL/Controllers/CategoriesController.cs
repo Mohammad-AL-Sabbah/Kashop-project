d45a5d08bc41c08caa67136012115b57874b50ab
Mohammad-AL-Sabbah <sozdizo587@gmail.com>
2025-11-19 17:06:12 +0200

    first

---
+++ KASOP.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using KASHOP.DAL.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KASOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context ;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+                       _context = context;
+        }
+
+    }
+}
